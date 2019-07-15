8f952a9b464c0cbc491b122cb519d3a5fcef2997
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-07-15 08:34:24 +0300

    add socket connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include <sys/socket.h>
+#include <iostream>
+
 int main(int argc, char** argv)
 {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0); // create socket
+
+    if (sockfd < 0) {
+        std::cout << "Err create socket" << std::endl;
+        return 1;
+    }
+
+    struct sockaddr serv_addr;
+    serv_addr.sa_family = AF_INET;
+
+    bind(sockfd, &serv_addr, sizeof(serv_addr));
+
     return 0;
 }
